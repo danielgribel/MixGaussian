2447769f5e69ec7cb204feaaaf77785a44a7a3c2
Daniel Lemes Gribel <gribel.daniel@gmail.com>
2017-09-16 18:50:07 -0300

    refactoring of var names

--- em.R
+++ em.R
@@ -4,28 +4,28 @@ nbPoints <- 1000
 
 nbClusters <- 3
 
-alphas <- runif(nbClusters)
+y_alphas <- runif(nbClusters)
 
-alphas <- alphas/sum(alphas)
+y_alphas <- y_alphas/sum(y_alphas)
 
-means <- sample.int(10, nbClusters, replace = FALSE)
+y_means <- sample.int(10, nbClusters, replace = FALSE)
 
 x <- rep(0, nbPoints)
 
-s <- sample(seq(1:nbClusters), size = nbPoints, replace = TRUE, prob = alphas)
+s <- sample(seq(1:nbClusters), size = nbPoints, replace = TRUE, prob = y_alphas)
 
 for( i in 1:nbPoints ) {
-	x[i] <- rnorm(1, mean = means[s[i]])
+	x[i] <- rnorm(1, mean = y_means[s[i]])
 }
 
 # simulating an outlier
 x[1] <- 50.0
 
 # initial guess for distributions means
-mu <- sample.int(10, nbClusters, replace = FALSE)
+means <- sample.int(10, nbClusters, replace = FALSE)
 
 # initial guess for proportion of distributions (guess for alphas)
-tau <- rep(1.0/nbClusters, nbClusters)
+alphas <- rep(1.0/nbClusters, nbClusters)
 
 T <- array(dim = c(nbClusters, nbPoints))
 P <- array(dim = c(nbClusters, nbPoints))
@@ -34,18 +34,18 @@ for(i in 1:100) {
 	
 	# Given the observed data, as well as the distribution parameters, what are the latent variables?
 	for(j in 1:nbClusters) {
-		T[j,] <- tau[j] * dnorm( x, mu[j] )
+		T[j,] <- alphas[j] * dnorm( x, means[j] )
 	}
 
 	for(j in 1:nbClusters) {
 		P[j,] <- T[j,] / colSums (T, dims = 1)
-		tau[j] <- mean(P[j,], na.rm=TRUE)
+		alphas[j] <- mean(P[j,], na.rm=TRUE)
 
 		# Given the observed data, as well as the latent variables, what are the population parameters?
-		mu[j] <- sum((P[j,] * x), na.rm=TRUE) / sum(P[j,], na.rm=TRUE)
+		means[j] <- sum((P[j,] * x), na.rm=TRUE) / sum(P[j,], na.rm=TRUE)
 	}
 
-	# print(mu)
+	# print(means)
 }
 
 f_points <- colSums(T, dims=1)
