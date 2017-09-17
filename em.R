8acd476119cd828d1a03ef9231ced2a7c443221c
Daniel Lemes Gribel <gribel.daniel@gmail.com>
2017-09-17 12:50:33 -0300

    some comments

--- em.R
+++ em.R
@@ -4,21 +4,25 @@ nbPoints <- 1000
 
 nbClusters <- 3
 
+# real alphas (proportion of each distribution in the entire population)
 y_alphas <- runif(nbClusters)
-
 y_alphas <- y_alphas/sum(y_alphas)
 
+# real means of the distributions -- means are random in the interval 1..10
 y_means <- sample.int(10, nbClusters, replace = FALSE)
 
+# data points
 x <- rep(0, nbPoints)
 
+# choose the distribution from which the point came according to real alphas
 s <- sample(seq(1:nbClusters), size = nbPoints, replace = TRUE, prob = y_alphas)
 
+# generates data points according to the chosen distribution
 for( i in 1:nbPoints ) {
 	x[i] <- rnorm(1, mean = y_means[s[i]])
 }
 
-# simulating an outlier
+# simulating an outlier -- out of interval 0..10
 x[1] <- 50.0
 
 # initial guess for distributions means
@@ -32,16 +36,17 @@ P <- array(dim = c(nbClusters, nbPoints))
 
 for(i in 1:100) {
 	
-	# Given the observed data, as well as the distribution parameters, what are the latent variables?
+	# given the observed data and the distribution parameters, estimate the prob of data point coming from each distribution
 	for(j in 1:nbClusters) {
 		T[j,] <- alphas[j] * dnorm( x, means[j] )
 	}
 
 	for(j in 1:nbClusters) {
+		# normalize the density prob
 		P[j,] <- T[j,] / colSums (T, dims = 1)
-		alphas[j] <- mean(P[j,], na.rm=TRUE)
 
-		# Given the observed data, as well as the latent variables, what are the population parameters?
+		# given the observed data and the density prob, estimate the population parameters (alphas and means)
+		alphas[j] <- mean(P[j,], na.rm=TRUE)
 		means[j] <- sum((P[j,] * x), na.rm=TRUE) / sum(P[j,], na.rm=TRUE)
 	}
 
